9c0cb3d15870d2eab934de6c70d6b3d916de3472
Matúš Brliť <matus.brlit@gmail.com>
2015-12-27 20:52:46 +0100

    phantom js 1.9

--- karma.conf.js
+++ karma.conf.js
@@ -54,7 +54,7 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['PhantomJS2'],
+        browsers: ['PhantomJS'],
 
         customLaunchers: {
             Chrome_travis_ci: {
