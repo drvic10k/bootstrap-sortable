b3a8944389cc8926f4d8fdb26db1a82c29e8eee7
Matúš Brliť <matus.brlit@gmail.com>
2015-12-27 21:00:34 +0100

    config cleanup

--- karma.conf.js
+++ karma.conf.js
@@ -56,21 +56,10 @@ module.exports = function (config) {
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['PhantomJS'],
 
-        customLaunchers: {
-            Chrome_travis_ci: {
-                base: 'Chrome',
-                flags: ['--no-sandbox']
-            }
-        },
-
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
         singleRun: true
     };
-/*
-    if (process.env.TRAVIS) {
-        configuration.browsers = ['Chrome_travis_ci'];
-    }*/
 
     config.set(configuration);
 }
